5f1a4e03c17a1e9e8c54d72827bb801d0d22a047
Alex Testa <alex.testa@studio.unibo.it>
2024-11-29 22:09:23 +0100

    Refactor

---
+++ env/env.py
@@ -0,0 +1,226 @@
+import random as rnd
+import numpy as np
+import gymnasium as gym
+from gymnasium import spaces
+import carla
+import debug_utility
+
+
+class CarlaEnv(gym.Env):
+    
+    TARGET_SPEED = 16.6667 #60 km/h
+    MAX_RELATIVE_SPEED = 100
+
+    def __init__(self):
+        super(CarlaEnv, self).__init__()
+
+        #Set up the CARLA client
+        self.client = carla.Client('localhost', 2000)
+        self.client.set_timeout(10.0)
+        self.world = self.client.load_world('Town04')
+
+        #Define the action for ego vehicle [throttle, break]
+        self.action_space = spaces.Box(
+            low=np.array([0.0, 0.0]), # [min(throttle), min(break)]
+            high=np.array([1.0, 1.0]) # [max(throttle), max(break)]
+        )
+
+        #Define the observation space
+        self.observation_space = spaces.Box(
+            low=np.array([0.0, 0.0, 0.0]), # [min(distance), min(ego_speed), min(leader_speed)] in m/s
+            high=np.array([100.0, 30.0, self.MAX_RELATIVE_SPEED]), # [max(distance, max(ego_speed), max(leader_speed)] in m/s
+            dtype=np.float32
+        )
+
+        #Configure the default veicle
+        self.blueprint_library = self.world.get_blueprint_library()
+        self.vehicle_bp = self.blueprint_library.filter('vehicle.tesla.model3')[0]
+        self.spawn_points = self.world.get_map().get_spawn_points()
+
+        self.ego_vehicle = None
+        self.leader_vehicle = None
+        self.radar_sensor = None
+
+        self.radar_data = {  # Default radar data
+            "distance": 50,  # Default distance if no object is detected
+            "relative_speed": self.MAX_RELATIVE_SPEED  # Default relative speed
+        }
+
+        #For oscillation
+        self.previous_action = 0.0
+
+    def spawn_vehicles(self):
+        spawn_point_leader = self.spawn_points[1]
+        self.leader_vehicle = self.world.spawn_actor(self.vehicle_bp, spawn_point_leader)
+
+        # Spawn point ego vehicle at random position
+        safe_distance = (self.TARGET_SPEED / 10)**2 # To avoid collisions
+        random_distance = rnd.uniform(safe_distance + 1, 100.0)
+
+        ego_vehicle = carla.Location(
+            x = spawn_point_leader.location.x - random_distance,
+            y = spawn_point_leader.location.y,
+            z = spawn_point_leader.location.z
+        )
+
+        ego_rotation = spawn_point_leader.rotation
+
+        self.ego_vehicle = self.world.spawn_actor(
+            self.vehicle_bp, 
+            carla.Transform(ego_vehicle, ego_rotation))
+
+    
+    def reset(self, seed=42):
+        # Destroy existing actors
+        if self.leader_vehicle is not None:
+            self.leader_vehicle.destroy()
+        if self.ego_vehicle is not None:
+            self.ego_vehicle.destroy()
+        if self.radar_sensor is not None:
+            self.radar_sensor.destroy()
+            self.radar_sensor = None 
+        
+        # Spawn vehicles
+        self.spawn_vehicles()
+
+        #Add radar sensor
+        radar_bp = self.blueprint_library.find('sensor.other.radar')
+        radar_bp.set_attribute('horizontal_fov', '2')
+        radar_bp.set_attribute('vertical_fov', '2')
+        radar_bp.set_attribute('range', '50')  # Maximum range of the radar
+
+        self.radar_sensor = self.world.spawn_actor(
+            radar_bp,
+            carla.Transform(carla.Location(x=2.5, z=1.0)),
+            attach_to=self.ego_vehicle
+        )
+
+        # Get data from radar sensor
+        self.radar_sensor.listen(self._radar_callback)
+
+        # Reset veichle state
+        random_velocity = rnd.uniform(2.7778, self.TARGET_SPEED + 1) # ~ min: 10km/h max: 60km/h 
+        self.ego_vehicle.set_target_velocity(carla.Vector3D(random_velocity, 0, 0))
+        self.leader_vehicle.set_target_velocity(carla.Vector3D(0, 0, 0))
+        
+        # Wait for simulation to progress
+        self.world.tick()
+
+        # Return initial observation
+        return self._get_observation(), {}
+    
+    def _radar_callback(self, radar_data):
+        distances = []
+        velocities = []
+
+        for detection in radar_data:
+            debug_utility.draw_radar_point(self.radar_sensor, detection)
+            distances.append(detection.depth) #Distance to detect object
+            velocities.append(detection.velocity) #Velocity of detect object
+        
+        # Use the closest detected object as the relevant one for control
+        if distances:
+            self.radar_data["distance"] = min(distances)
+            self.radar_data["relative_speed"] = velocities[np.argmin(distances)]
+        else:
+            # No object detected, set default values
+            self.radar_data["distance"] = 50
+            self.radar_data["relative_speed"] = self.MAX_RELATIVE_SPEED
+
+    def step(self, action):
+
+        throttle = float(max(0.0, action[0]))
+        brake = float(max(0.0, action[1]))
+
+        # Apply action to throttle and brake
+        control = carla.VehicleControl()
+        control.throttle = throttle
+        control.brake = brake
+        self.ego_vehicle.apply_control(control)
+
+        self.world.tick()
+
+        observation = self._get_observation()
+        reward = self._compute_reward(observation, action)
+        done = self._check_done(observation)
+
+        print(f"Throttle: {throttle}, Brake: {brake}, Reward: {reward}")
+
+        return observation, reward, done, False, {}
+    
+    def _get_observation(self):
+        distance = self.radar_data["distance"]
+        relative_speed = self.radar_data["relative_speed"]
+
+        ego_speed = np.linalg.norm([
+            self.ego_vehicle.get_velocity().x,
+            self.ego_vehicle.get_velocity().y,
+            self.ego_vehicle.get_velocity().z
+        ])
+
+        return np.array([abs(distance), abs(ego_speed), abs(relative_speed)], dtype=np.float32)
+
+    def _compute_reward(self, observation, action):
+        distance, ego_speed, leader_speed = observation # Leader speed is relative to ego vehicle
+
+        tolerance = 1.0
+        absolute_speed = ego_speed + leader_speed
+        collision_time = distance / leader_speed
+
+        # Reward
+        reward = 0
+
+        #if no object detected mantain TARGET SPEED
+        if leader_speed == self.MAX_RELATIVE_SPEED:
+            if self.TARGET_SPEED - tolerance <= ego_speed <= self.TARGET_SPEED + tolerance: 
+                reward += 1
+            else: reward += -1
+        else:
+            if collision_time > 5.0 and absolute_speed - 1 < ego_speed < absolute_speed + 1:
+                reward += 1
+            else:
+                #if object is detected and 2.4 <= collision time <= 5.0  and absolute speed - 1 < ego speed < absolute speed - 1
+                if 2.4 <= collision_time <= 5.0:
+                    if absolute_speed - tolerance < ego_speed < absolute_speed - tolerance:
+                        reward += 5
+                    else: 
+                        reward += -5
+                #if object is detected and but collision time < 1.0
+                if collision_time < 1.0: #Collisione
+                    reward += -100
+        
+        # Penality for oscillation
+        if abs(self.previous_action - action[0]) > 0.70:
+            reward -= 5
+        else:
+            reward += 5
+        
+        if abs(self.previous_action - action[1]) > 0.80:
+            reward -= 5
+        else:
+            reward += 5
+        
+        return max(reward, -200) # Avoid large negative rewards
+
+
+
+    def _check_done(self, observation):
+        distance, ego_speed, leader_speed = observation
+
+        safe_distance = (self.TARGET_SPEED / 10)**2 # To avoid collisions
+        collision_time = distance / leader_speed
+        ego_stopped = abs(ego_speed) < 0.1
+
+        if distance >= safe_distance and ego_stopped:
+            print("Ego vehicle is too longer than safe distance meters from the ego vehicle.")
+            return True
+
+        if ego_stopped:
+            print("Ego vehicle has stopped.")
+            return True
+        
+        if collision_time < 1.0:
+            print("Collision time detected.")
+            return True
+        
+        return False
--- train.py
+++ train.py
@@ -1,43 +1,29 @@
 import os
 import gymnasium as gym
 from stable_baselines3 import PPO
-from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
-from env import CarlaEnv
-# Define paths to save models and logs
-log_dir = "./ppo_logs/"
-os.makedirs(log_dir, exist_ok=True)
+from stable_baselines3.common.env_checker import check_env
+from env.env import CarlaEnv
+
+# Trained environmentw
 env = CarlaEnv()
+
+check_env(env) # check environment
+
+
 # Initialize the PPO model
 model = PPO(
     "MlpPolicy",  # Use an MLP (feed-forward neural network) policy
     env,
     verbose=1,
-    tensorboard_log=log_dir,  # Log data for TensorBoard visualization
     gamma=0.99,  # Discount factor
     learning_rate=0.0001,  # Learning rate
     n_steps=2048,  # Rollout buffer size
     batch_size=64,  # Batch size for training
     ent_coef=0.01,  # Entropy coefficient
 )
-# Set up a checkpoint callback to save models during training
-checkpoint_callback = CheckpointCallback(
-    save_freq=10000,  # Save every 10,000 steps
-    save_path="./ppo_checkpoints/",
-    name_prefix="ppo_carla"
-)
-
-# Evaluate the trained model during training and save the best model
-eval_callback = EvalCallback(
-    env,
-    best_model_save_path="./ppo_best_model/",
-    log_path="./ppo_eval_logs/",
-    eval_freq=5000,
-    deterministic=True,
-    render=False
-)
-
 # Train the PPO model
-model.learn(total_timesteps=500000, callback=[checkpoint_callback, eval_callback])
+model.learn(total_timesteps=500000)
+
 # Save the final model
 model.save("ppo_carla_model")
 # Clean up the environment
