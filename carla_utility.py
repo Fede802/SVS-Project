3f9e5e7cb3c53026f513b2c1c02a862c87a47404
Federico Bravetti - federico.bravetti2@studio.unibo.it <federico.bravetti2@studio.unibo.it>
2024-12-01 17:37:39 +0100

    refactor

--- carla_utility.py
+++ carla_utility.py
@@ -5,76 +5,62 @@ def __spawn_actor(world, blueprint, spawn_point, attach_to = None):
     time.sleep(2)
     return actor
 
-def move_spectator_to(world, spectator, to, distance=5.0, transform = carla.Transform(carla.Location(x = 5, z=5), carla.Rotation(pitch=-10))):
-    back_location = to.location - to.get_forward_vector() * distance
-    back_location.x += transform.location.x
-    back_location.y += transform.location.y
-    back_location.z += transform.location.z
-    to.rotation.yaw += transform.rotation.yaw
-    to.rotation.pitch = transform.rotation.pitch
-    to.rotation.roll = transform.rotation.roll
-    spectator_transform = carla.Transform(back_location, to.rotation)
-    spectator.set_transform(spectator_transform)
-    #world.tick()
+def __transform_vector(point: carla.Transform, transform: carla.Transform):
+    point.location.x += transform.location.x
+    point.location.y += transform.location.y
+    point.location.z += transform.location.z
+    point.rotation.pitch += transform.rotation.pitch
+    point.rotation.roll += transform.rotation.roll
+    point.rotation.yaw += transform.rotation.yaw
+    return point
 
-def spawn_veichle_bp_at(world, vehicle, spawn_point=carla.Transform(), transform = carla.Transform()):
+def move_spectator_to(spectator, to, distance=5.0, transform = carla.Transform(carla.Location(x = 5, z=5), carla.Rotation(pitch=-10))):
+    spectator_transform = carla.Transform(carla.Location(to.location - to.get_forward_vector() * distance), to.rotation)
+    spectator.set_transform(__transform_vector(spectator_transform, transform))
+    
+def spawn_vehicle_bp_at(world, vehicle, spawn_point=carla.Transform(), transform = carla.Transform()):
     blueprint_library = world.get_blueprint_library()
-    vehicle_bp = blueprint_library.find(vehicle)   
-    spawn_point.location.x += transform.location.x
-    spawn_point.location.y += transform.location.y
-    spawn_point.location.z += transform.location.z
-    spawn_point.rotation.pitch += transform.rotation.pitch
-    spawn_point.rotation.roll += transform.rotation.roll
-    spawn_point.rotation.yaw += transform.rotation.yaw
-    return __spawn_actor(world, vehicle_bp, spawn_point)
+    vehicle_bp = blueprint_library.find(vehicle) 
+    return __spawn_actor(world, vehicle_bp, __transform_vector(spawn_point, transform))
 
-def spawn_veichle_at(world, vehicle_index=0, spawn_point=carla.Transform(), pattern='vehicle.*', transform = carla.Transform()):
+def spawn_vehicle_at(world, vehicle_index=0, spawn_point=carla.Transform(), pattern='vehicle.*', transform = carla.Transform()):
     blueprint_library = world.get_blueprint_library()
     vehicle_bp = blueprint_library.filter(pattern)[vehicle_index]   
-    spawn_point.location.x += transform.location.x
-    spawn_point.location.y += transform.location.y
-    spawn_point.location.z += transform.location.z
-    spawn_point.rotation.pitch += transform.rotation.pitch
-    spawn_point.rotation.roll += transform.rotation.roll
-    spawn_point.rotation.yaw += transform.rotation.yaw
-    return __spawn_actor(world, vehicle_bp, spawn_point)
+    return __spawn_actor(world, vehicle_bp, __transform_vector(spawn_point, transform))
 
-def spawn_veichle_in_front_of(world, veichle, vehicle_index=0, offset=0):
-    v3d = debug_utility.get_point_at(veichle, offset)
-    return spawn_veichle_at(world,vehicle_index,spawn_point=carla.Transform(carla.Location(v3d.x, v3d.y, v3d.z + 10), veichle.get_transform().rotation))
+def spawn_vehicle_in_front_of(world, vehicle: carla.Actor, vehicle_index=0, offset=0):
+    v3d = debug_utility.get_point_at(vehicle, vehicle.get_location(), offset)
+    #+10 on z is to avoid spawn bug
+    return spawn_vehicle_at(world,vehicle_index,spawn_point=carla.Transform(carla.Location(v3d.x, v3d.y, v3d.z + 10), vehicle.get_transform().rotation))
 
-def spawn_veichle_bp_in_front_of(world, veichle, vehicle_bp_name, offset=0):
-    v3d = debug_utility.get_point_at(veichle, veichle.get_location(), offset)
-    return spawn_veichle_bp_at(world,vehicle_bp_name,spawn_point=carla.Transform(carla.Location(v3d.x, v3d.y, v3d.z + 10), veichle.get_transform().rotation))
+def spawn_vehicle_bp_in_front_of(world, vehicle, vehicle_bp_name, offset=0):
+    v3d = debug_utility.get_point_at(vehicle, vehicle.get_location(), offset)
+    #+10 on z is to avoid spawn bug
+    return spawn_vehicle_bp_at(world,vehicle_bp_name,spawn_point=carla.Transform(carla.Location(v3d.x, v3d.y, v3d.z + 10), vehicle.get_transform().rotation))
 
 def spawn_vehicle(world, vehicle_index=0, spawn_index=0, pattern='vehicle.*', transform = carla.Transform()):
     blueprint_library = world.get_blueprint_library()
     vehicle_bp = blueprint_library.filter(pattern)[vehicle_index]
     spawn_point = world.get_map().get_spawn_points()[spawn_index]
-    spawn_point.location.x += transform.location.x
-    spawn_point.location.y += transform.location.y
-    spawn_point.location.z += transform.location.z
-    spawn_point.rotation.pitch += transform.rotation.pitch
-    spawn_point.rotation.roll += transform.rotation.roll
-    spawn_point.rotation.yaw += transform.rotation.yaw
-    return __spawn_actor(world, vehicle_bp, spawn_point)
+    return __spawn_actor(world, vehicle_bp, __transform_vector(spawn_point, transform))
 
 def spawn_camera(world, attach_to=None, transform=carla.Transform(carla.Location(x=1.2, z=1.2), carla.Rotation(pitch=-10)), width=800, height=600):
     camera_bp = world.get_blueprint_library().find('sensor.camera.rgb')
     camera_bp.set_attribute('image_size_x', str(width))
     camera_bp.set_attribute('image_size_y', str(height))
-    #camera_bp.set_attribute('fov', '120')
-    #camera_bp.set_attribute('sensor_tick', '0')
     return __spawn_actor(world, camera_bp, transform, attach_to=attach_to)
 
-def spawn_radar(world, attach_to, transform=carla.Transform(carla.Location(x=1.2, z=2), carla.Rotation(pitch=0)), range=100, points_per_second=1000):
+def spawn_radar(world, attach_to, transform=carla.Transform(carla.Location(x=1.2, z=2.2), carla.Rotation(pitch=0)), horizontal_fov = 30, vertical_fov = 30, range=100, points_per_second=1500, sensor_tick = 0):
     radar_bp = world.get_blueprint_library().find('sensor.other.radar')
-    radar_bp.set_attribute('horizontal_fov', '30')
-    radar_bp.set_attribute('vertical_fov', '30')
+    radar_bp.set_attribute('horizontal_fov', str(horizontal_fov))
+    radar_bp.set_attribute('vertical_fov', str(vertical_fov))
     radar_bp.set_attribute('range', str(range))
-    #radar_bp.set_attribute('points_per_second', str(points_per_second))
+    radar_bp.set_attribute('points_per_second', str(points_per_second))
+    radar_bp.set_attribute('sensor_tick', str(sensor_tick))
     return __spawn_actor(world, radar_bp, transform, attach_to=attach_to)
 
-def draw_on_screen(world, transform, content='O', color=carla.Color(0, 255, 0), life_time=0.1):
-    world.debug.draw_string(transform.location, content, color=color, life_time=life_time)   
-
+def destroy_all_vehicle_and_sensors(world: carla.World):
+    for v in world.get_actors().filter('vehicle.*'):
+        v.destroy()
+    for v in world.get_actors().filter('sensor.*'):
+        v.destroy()
--- main.py
+++ main.py
@@ -1,58 +1,42 @@
-import carla, time, pygame, math, random, cv2
-import open3d as o3d
-from matplotlib import cm
+import carla, time, pygame, cv2, debug_utility, carla_utility
 import numpy as np
-import debug_utility
-import carla_utility
-
 
 client = carla.Client('localhost', 2000)
 print(client.get_available_maps())
-#time.sleep(1000)
 #3 bella dritta, 4 anche meglio
 client.load_world('Town04')
-client.set_timeout(50.0)
+client.set_timeout(100.0)
 
 world = client.get_world()
 spectator = world.get_spectator()
 
+carla_utility.destroy_all_vehicle_and_sensors(world) #to avoid spawning bugs
 
-
-target_velocity = 50
-
-#TTC = (distance / relative_velocity) if relative_velocity != 0 else 9999
-# Simple callback function to print the number of detections
-min_ttc = float('inf')
+radar_detection_h_radius = 1
+radar_detection_v_radius = 1.3
+target_velocity = 70
+min_permitted_distance = 50
 
 def handle_measurement(data: carla.RadarMeasurement, radar: carla.Actor):
-    global min_ttc, min_distance
-    min_ttc = float('inf')
+    global min_ttc, min_depth
+    min_ttc = min_depth = float('inf')
     for detection, i in zip(data, range(len(data))):
         absolute_speed = abs(detection.velocity)
-        #debug_utility.draw_radar_point(radar, detection, color = carla.Color(255, 0, 255))
-        # Calculate TTC
-        if debug_utility.evaluate_point(radar, detection, 1, 1):
+        if debug_utility.evaluate_point(radar, detection, radar_detection_h_radius, radar_detection_v_radius):
             debug_utility.draw_radar_point(radar, detection)
             if absolute_speed != 0:
-                
-                #print(detection)
-                #time.sleep(5)
                 ttc = detection.depth / absolute_speed
                 if ttc < min_ttc:
                     min_ttc = ttc
+            elif detection.depth < min_permitted_distance:
+                if detection.depth < min_depth:
+                    min_depth = detection.depth
 
 pygame.init()
 pygame.display.set_mode((400, 300))
 
-for v in world.get_actors().filter('vehicle.*'):
-    v.destroy()
-
-#ego_vehicle = carla_utility.spawn_vehicle(world=world, vehicle_index=15, transform=carla.Transform(rotation = carla.Rotation(yaw=45)))
-#ego_vehicle = carla_utility.spawn_vehicle(world=world, vehicle_index=15, spawn_index=3)
-#ego_vehicle = carla_utility.spawn_veichle_at(world=world, vehicle_index=15, spawn_point=carla.Transform(carla.Location(x=2.484849, y=-170.415253, z=2.900956)))
-ego_vehicle = carla_utility.spawn_veichle_bp_at(world=world, vehicle='vehicle.tesla.cybertruck', spawn_point=carla.Transform(carla.Location(x=380.786957, y=31.491543, z=13.309415), carla.Rotation(yaw = 180)))
-#other_vehicle = carla_utility.spawn_vehicle(world=world, transform=carla.Transform(carla.Location(x=50)))
-other_vehicle = carla_utility.spawn_veichle_bp_in_front_of(world, ego_vehicle, vehicle_bp_name='vehicle.tesla.cybertruck', offset=40)
+ego_vehicle = carla_utility.spawn_vehicle_bp_at(world=world, vehicle='vehicle.tesla.cybertruck', spawn_point=carla.Transform(carla.Location(x=380.786957, y=31.491543, z=13.309415), carla.Rotation(yaw = 180)))
+other_vehicle = carla_utility.spawn_vehicle_bp_in_front_of(world, ego_vehicle, vehicle_bp_name='vehicle.tesla.cybertruck', offset=40)
 
 radar = carla_utility.spawn_radar(world, ego_vehicle, range=70)
 radar.listen(lambda data: handle_measurement(data, radar))
@@ -62,62 +46,28 @@ def camera_callback(image):
     global video_output
     video_output = np.reshape(np.copy(image.raw_data), (image.height, image.width, 4))
 
-
-
-
-
-def compute_controls(velocita_corrente, velocita_target, ttc, k_p=0.05):
-    """
-    Calcola throttle e brake in base alla velocità corrente, velocità target e TTC.
-    
-    Args:
-        velocita_corrente (float): Velocità attuale del veicolo in m/s.
-        velocita_target (float): Velocità desiderata in m/s.
-        ttc (float): Time to collision in secondi.
-        k_p (float): Fattore di proporzionalità per il controllo.
-        
-    Returns:
-        throttle (float): Valore tra 0.0 e 1.0.
-        brake (float): Valore tra 0.0 e 1.0.
-    """
+def compute_controls(velocita_corrente, velocita_target):
     errore_vel = velocita_target - (velocita_corrente * 3.6)
-    #print(errore_vel)
-
     errore_vel_perc = abs(errore_vel) * 100 / velocita_target
     
-    # Inizializza throttle e brake
     throttle = 0.0
     brake = 0.0
-    #print(min_ttc)
-    if min_ttc > 0 and min_ttc < float('inf'):
-        #print(min_ttc)
+    
+    if min_depth < min_permitted_distance or (min_ttc > 0 and min_ttc < float('inf')):
         brake = 1.0
     else:    
         if errore_vel > 0:
             throttle = 1.0 * errore_vel_perc
         else:
-            brake = 1.0 * errore_vel_perc    
-        # Accelerazione
-     #   throttle = min(0.5 + k_p * errore_vel, 1.0)
-    #elif errore_vel < 0:
-        # Decelerazione
-     #   brake = min(-k_p * errore_vel, 1.0)
+            brake = 1.0 * errore_vel_perc
 
-    #if brake > 0:
-     #   throttle = 0.0
-    
-    # TTC Safety Override
-    #if ttc < 1.5:
-     #   brake = 1.0
-      #  throttle = 0.0
-    
     return throttle, brake
 
-carla_utility.move_spectator_to(world, spectator, ego_vehicle.get_transform())
+carla_utility.move_spectator_to(spectator, ego_vehicle.get_transform())
 show_in_carla = False
 show_in_camera = True
 running = True
-cruise_control = False
+cruise_control = True
 lastUpdate = 0
 
 if show_in_camera:
@@ -128,16 +78,16 @@ if show_in_camera:
 try:
     while running:
         control = carla.VehicleControl()
-        other_vehicle.apply_control(carla.VehicleControl(brake=1.0))
-        #print(spectator.get_location())
-        #control.throttle = 1.0
-
+        other_vehicle.apply_control(carla.VehicleControl(throttle=0.3))
+        
         debug_utility.draw_radar_bounding_range(radar)
-        debug_utility.draw_radar_point_cloud_range(radar, 1, 1)
+        debug_utility.draw_radar_point_cloud_range(radar, radar_detection_h_radius, radar_detection_v_radius)
+        
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.QUIT or event.key == pygame.K_ESCAPE:
                     running = False
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             control.throttle = 0.5
@@ -154,33 +104,25 @@ try:
             cruise_control = True
         if keys[pygame.K_p]:
             cruise_control = False 
+        
         if cruise_control:              
-            throttle, brake = compute_controls(ego_vehicle.get_velocity().length(), target_velocity, min_ttc)
+            throttle, brake = compute_controls(ego_vehicle.get_velocity().length(), target_velocity)
             control = carla.VehicleControl(throttle=throttle, brake=brake)
-            #print(control)
         ego_vehicle.apply_control(control)
         
-
         if show_in_carla:
-            carla_utility.move_spectator_to(world, spectator, ego_vehicle.get_transform())
+            carla_utility.move_spectator_to(spectator, ego_vehicle.get_transform())
         if show_in_camera:
             cv2.imshow('RGB Camera', video_output)
         pygame.display.flip()
-        #print("Actor transform: ", ego_vehicle.get_transform())
-        #print("Actor forward vector (direction):", ego_vehicle.get_transform().get_forward_vector())
-        #print("Actor control: ",ego_vehicle.get_control().throttle," ", ego_vehicle.get_control().brake," Actor velocity: ", ego_vehicle.get_velocity(),", ",ego_vehicle.get_velocity().length(),"m/s, ",ego_vehicle.get_velocity().length()*3.6,"km/h")
-        
+       
         if(time.time() - lastUpdate > 0.2):
-            #vPlot.add_value(ego_vehicle.get_velocity().length()*3.6)
-            #aPlot.add_value(ego_vehicle.get_acceleration().length())
             lastUpdate = time.time()
         world.tick()
 except KeyboardInterrupt:
-    pass 
+    pass
 finally:
     pygame.quit()
-    cv2.destroyAllWindows()
-    radar.stop()
-    radar.destroy()
-    ego_vehicle.destroy()
-    other_vehicle.destroy()     
+    cv2.destroyAllWindows()  
+    carla_utility.destroy_all_vehicle_and_sensors(world)
+  
