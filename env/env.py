c2bcc831fecd0584757cb888d605f763ac06f233
Alex Testa <alex.testa@studio.unibo.it>
2024-12-10 12:48:02 +0100

    Refactor

--- env/env.py
+++ env/env.py
@@ -4,13 +4,15 @@ import gymnasium as gym
 from gymnasium import spaces
 import carla
 import debug_utility
+import carla_utility as utility
 import math
 
 class AccEnv(gym.Env):
     
     TARGET_SPEED = 16.6667 #60 km/h
     SPAWN_POINT = carla.Location(x=2393, y=6000, z=167)
-    spawn_yaw = -88.2
+    SPAWN_YAW = -88.2
+    VEHICLE_BP = 'vehicle.tesla.model3'
 
     def __init__(self):
         super(AccEnv, self).__init__()
@@ -19,27 +21,20 @@ class AccEnv(gym.Env):
         self.client = carla.Client('localhost', 2000)
         self.client.set_timeout(100.0)
         self.world = self.client.get_world()
+        
+        #TODO: Refactor Spectator
         self.spectator = self.world.get_spectator()
-        self.spectator.set_transform(carla.Transform(self.SPAWN_POINT, carla.Rotation(yaw = self.spawn_yaw)))
+        self.spectator.set_transform(carla.Transform(self.SPAWN_POINT, carla.Rotation(yaw = self.SPAWN_YAW)))
 
         #Define the action for ego vehicle [throttle, break]
-        self.action_space = spaces.Box(
-            low=np.array([0.0, 0.0]), # [min(throttle), min(break)]
-            high=np.array([1.0, 1.0]), # [max(throttle), max(break)]
-            dtype=np.float64
-        )
+        self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float64)
 
         #Define the observation space
         self.observation_space = spaces.Box(
-            low=np.array([0.0, 0.0, -30.0, self.TARGET_SPEED, 0]), # [min(distance), min(ego_speed), min(relative_speed), target_speed, object detected] in m/s
-            high=np.array([50, 30.0, 30.0, self.TARGET_SPEED, 1]), # [max(distance, max(ego_speed), max(relative_speed), target_speed, object detected] in m/s
+            low=np.array([0.0, 0.0, 0.0, -30.0, 0, 1]), # [min(distance), min(ego_speed), min(leader_speed), min(relative_speed), object detected, security_distance] in m/s
+            high=np.array([50, 30.0, 30.0, 30.0, 1, 100]), # [max(distance, max(ego_speed), max(leader_speed), max(relative_speed), object detected, security_distance] in m/s
             dtype=np.float64
         )
-
-        #Configure the default vehicle 
-        self.blueprint_library = self.world.get_blueprint_library()
-        self.vehicle_bp = self.blueprint_library.filter('vehicle.tesla.model3')[0]
-        self.spawn_points = self.world.get_map().get_spawn_points()
 
         self.ego_vehicle = None
         self.leader_vehicle = None
@@ -53,63 +48,38 @@ class AccEnv(gym.Env):
 
 
     def spawn_vehicles(self):
-        spawn_point_leader = carla.Transform(self.SPAWN_POINT, carla.Rotation(yaw = self.spawn_yaw))
-        self.leader_vehicle = self.world.spawn_actor(self.vehicle_bp, spawn_point_leader)
+        #Spawn Ego Veichle
+        spawn_point_ego = carla.Transform(self.SPAWN_POINT, carla.Rotation(yaw = self.SPAWN_YAW))
+        self.ego_vehicle = utility.spawn_vehicle_bp_at(world=self.world, vehicle=self.VEHICLE_BP, spawn_point=spawn_point_ego)
 
         # Spawn point ego vehicle at random position
         random_distance = rnd.uniform(50.00, 100.0)
+        
+        #Spawn Leader Veichle
+        self.leader_vehicle = utility.spawn_vehicle_bp_in_front_of(self.world, self.ego_vehicle, self.VEHICLE_BP, offset=random_distance)
 
-        # TODO: Use utility function
-        ego_vehicle = carla.Location(
-            x = self.SPAWN_POINT.x - 1,
-            y = self.SPAWN_POINT.y + 50.0,
-            z = self.SPAWN_POINT.z
-        )
-
-        #print("Ego spawned at")
-        #print(f"x: {ego_vehicle.x}, y: {ego_vehicle.y}, z: {ego_vehicle.z}")
-
-        ego_rotation = spawn_point_leader.rotation
-
-        self.ego_vehicle = self.world.spawn_actor(
-            self.vehicle_bp, 
-            carla.Transform(ego_vehicle, ego_rotation))
 
     
     def reset(self, seed=42):
-        #print("----RESET----")
         # Destroy existing actors
-        if self.leader_vehicle is not None:
-            self.leader_vehicle.destroy()
-        if self.ego_vehicle is not None:
-            self.ego_vehicle.destroy()
-        if self.radar_sensor is not None:
-            self.radar_sensor.destroy()
-            self.radar_sensor = None 
+        utility.destroy_all_vehicle_and_sensors(world=self.world)
+        self.ego_vehicle = None
+        self.leader_vehicle = None
         
         # Spawn vehicles
         self.spawn_vehicles()
 
         #Add radar sensor
-        radar_bp = self.blueprint_library.find('sensor.other.radar')
-        radar_bp.set_attribute('horizontal_fov', '2')
-        radar_bp.set_attribute('vertical_fov', '2')
-        radar_bp.set_attribute('range', '50')  # Maximum range of the radar
-
-        self.radar_sensor = self.world.spawn_actor(
-            radar_bp,
-            carla.Transform(carla.Location(x=2.5, z=1.0)),
-            attach_to=self.ego_vehicle
-        )
+        self.radar_sensor = utility.spawn_radar(world=self.world, attach_to=self.ego_vehicle, transform=carla.Transform(carla.Location(x=2.5, z=1.0), carla.Rotation(pitch=0)), range=50, vertical_fov=5, horizontal_fov=5)
 
         # Get data from radar sensor
         self.radar_sensor.listen(self._radar_callback)
 
         # Reset vehicle state
-        random_ego_velocity = rnd.uniform(9.0, self.TARGET_SPEED) # ~ min: 32,4km/h max: 60km/h
-        random_leader_velocity = rnd.uniform(9.0, self.TARGET_SPEED + 5) # ~ min: 32,4km/h max: 60km/h
-        self.ego_vehicle.set_target_velocity(carla.Vector3D(random_ego_velocity*math.cos(self.spawn_yaw), random_ego_velocity*math.sin(self.spawn_yaw), 0))
-        self.leader_vehicle.set_target_velocity(carla.Vector3D(random_leader_velocity*math.cos(self.spawn_yaw), random_leader_velocity*math.sin(self.spawn_yaw),0))
+        random_ego_velocity = rnd.uniform(self.TARGET_SPEED - 5, self.TARGET_SPEED + 5)
+        random_leader_velocity = rnd.uniform(self.TARGET_SPEED - 5, self.TARGET_SPEED + 5)
+        self.ego_vehicle.set_target_velocity(carla.Vector3D(random_ego_velocity*math.cos(self.SPAWN_YAW), random_ego_velocity*math.sin(self.SPAWN_YAW), 0))
+        self.leader_vehicle.set_target_velocity(carla.Vector3D(random_leader_velocity*math.cos(self.SPAWN_YAW), random_leader_velocity*math.sin(self.SPAWN_YAW),0))
         
         # Wait for simulation to progress
         self.world.tick()
@@ -137,9 +107,18 @@ class AccEnv(gym.Env):
             self.radar_data["object_detected"] = 0
         
     def step(self, action):
-
-        throttle = float(max(0.0, action[0] - action[1]))
-        brake = float(max(0.0, action[1] - action[0]))
+        
+        action_value = action[0]
+        
+        if action_value > 0:
+            throttle = action_value
+            brake = 0.0
+        else:
+            brake = abs(action_value)
+            throttle = 0.0
+        
+        throttle = np.clip(throttle, 0.0, 1.0)
+        brake = np.clip(brake, 0.0, 1.0)
 
         # Apply action to throttle and brake
         control = carla.VehicleControl()
@@ -153,7 +132,7 @@ class AccEnv(gym.Env):
         reward = self._compute_reward(observation)
         done = self._check_done(observation)
 
-        print(f"Reward: {reward}")
+        print(f"Throttle: {throttle}, Brake: {brake}, Reward: {reward}")
         
 
         return observation, reward, done, False, {}
@@ -165,63 +144,35 @@ class AccEnv(gym.Env):
         object_detected = self.radar_data["object_detected"]
         
         ego_speed = self.ego_vehicle.get_velocity().length()
-        ego_speed = np.linalg.norm([
-            self.ego_vehicle.get_velocity().x,
-            self.ego_vehicle.get_velocity().y,
-            self.ego_vehicle.get_velocity().z
-        ])
+        leader_speed = abs(ego_speed - relative_speed)
+        security_distance = utility.compute_security_distance(ego_speed)
+        
+        return np.array([distance, abs(ego_speed), abs(leader_speed), relative_speed, object_detected, security_distance], dtype=np.float64)
 
-        return np.array([distance, abs(ego_speed), relative_speed, self.TARGET_SPEED, object_detected], dtype=np.float32)
-
-    #TODO: Reward min -1, reward max 1
+ 
     def _compute_reward(self, observation):
-        distance, ego_speed, relative_speed, target_speed, object_detected = observation # Leader speed is relative to ego vehicle
+        distance, ego_speed, leader_speed, relative_speed, object_detected, security_distance = observation
         
-        security_distance = (ego_speed / 10) ** 2
-        absolute_speed = abs(relative_speed) + abs(ego_speed)
-        reward = rnd.uniform(0.1, 0.5)
-
-        #No Object detected
-        if object_detected == 0:
-            
-            #Mantain target speed
-            if target_speed - 2 <= ego_speed <= target_speed + 2:
-                reward += 1
-            else:
-                reward -= 1 * abs(target_speed - ego_speed)
+        reward = 0
         
-        else:
-            #Object Detected
-            #Mantain security distance
-            if security_distance - 1 <= distance <= security_distance + 1:
-                reward += 1
-            else:
-                reward -= 1 * abs(security_distance - distance)
-            
-            # Leader velocity > target velocity
-            if target_speed <= absolute_speed:
-                #Mantain target speed
-                if target_speed - 2 <= ego_speed <= target_speed + 2:
-                    reward += 1
-                else:
-                    reward -= 1 * abs(target_speed - ego_speed)
-            else:
-                #Maintain leader velocity
-                if absolute_speed - 1 <= ego_speed <= absolute_speed + 1:
-                    reward += 1
-                else:
-                    reward -= 1 * abs(absolute_speed - ego_speed)
-                    
-            #Collision
-            if distance < 1:
-                reward -= 1
-            
-        return max(-1, reward)
+        if distance < 1:
+            reward -= 1
+        
+        if ego_speed < 0.1:
+            reward -= 0.5
+        
+        speed_error = abs(ego_speed - self.TARGET_SPEED)
+        reward += 1 - abs(speed_error / self.TARGET_SPEED)
+        
+        if object_detected == 1 and distance < security_distance:
+            reward -= 1 * (security_distance - distance) / security_distance
+        
+        return np.clip(reward, -1, 1)
 
 
 
     def _check_done(self, observation):
-        distance, ego_speed, _, _, object_detected = observation
+        distance, ego_speed, leader_speed, relative_speed, object_detected, security_distance = observation
 
         # Collision Detected
         if object_detected == 1 and distance <= 1:
