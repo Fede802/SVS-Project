9a95d301dbef964066134f5b2260ae00820f1026
Luigi Borriello - luigi.borriello2@studio.unibo.it <luigi.borriello2@studio.unibo.it>
2024-12-13 17:33:50 +0100

    refactor: make min-offset flexible

--- main_scheduled_adaptive.py
+++ main_scheduled_adaptive.py
@@ -23,7 +23,7 @@ radar_detection_v_radius = 0.8
 max_target_velocity = 130
 radar_range_offset = 20
 radar_range = carla_utility.compute_security_distance(max_target_velocity) + radar_range_offset
-target_velocity = 90 / 3.6
+target_velocity = 90
 min_distance_offset = 7
 min_permitted_distance = min_distance_offset
 learning_rate = 0.003
@@ -66,15 +66,15 @@ radar = carla_utility.spawn_radar(world, ego_vehicle, range=radar_range)
 radar.listen(lambda data: radar_callback(data, radar))
 carla_utility.move_spectator_to(spectator, ego_vehicle.get_transform())
 
-# other_vehicle = carla_utility.spawn_vehicle_bp_in_front_of(world, ego_vehicle, vehicle_bp_name='vehicle.tesla.cybertruck', offset=100)
+other_vehicle = carla_utility.spawn_vehicle_bp_in_front_of(world, ego_vehicle, vehicle_bp_name='vehicle.tesla.cybertruck', offset=100)
 
 if show_in_camera:
     camera = carla_utility.spawn_camera(world=world, attach_to=ego_vehicle, transform=carla.Transform(carla.Location(x=-6, z=5), carla.Rotation(pitch=-30)))
     camera.listen(lambda image: camera_callback(image))
     cv2.namedWindow('RGB Camera', cv2.WINDOW_AUTOSIZE)
 
-pid_controller = PIDController(learning_rate, update_frequency, min_distance_offset, buffer_size=None) #add buffer_size = None to disable buffer
-control_info = ControlInfo(pid_cc)
+pid_controller = PIDController(learning_rate, update_frequency, buffer_size=None) #add buffer_size = None to disable buffer
+control_info = ControlInfo(pid_cc, min_permitted_offset=min_distance_offset)
 logger = Logger()
 lastUpdate = 0
 
@@ -82,10 +82,10 @@ try:
     while control_info.running:
         # debug_utility.draw_radar_bounding_range(radar)
         # debug_utility.draw_radar_point_cloud_range(radar, radar_detection_h_radius, radar_detection_v_radius)
-
         if(time.time() - lastUpdate > update_frequency):
+            print(control_info)
             if control_info.pid_cc:
-                pid_controller.apply_control(control_info, target_velocity, ego_vehicle.get_velocity().length(), min_depth)           
+                pid_controller.apply_control(control_info, target_velocity, ego_vehicle.get_velocity().length() * 3.6, min_depth)           
             if send_info:
                 send_data({"velocity": ego_vehicle.get_velocity().length() * 3.6, "acceleration": ego_vehicle.get_acceleration().length()})
             if save_info:
@@ -101,7 +101,7 @@ try:
 
         # print(ego_vehicle.get_velocity().length()*3.6)
         ego_vehicle.apply_control(control_info.ego_control)
-        # other_vehicle.apply_control(control_info.target_control)
+        other_vehicle.apply_control(control_info.target_control)
         pygame.display.flip()
         world.tick()
 except KeyboardInterrupt:
--- pid_controller_scheduled_adaptive.py
+++ pid_controller_scheduled_adaptive.py
@@ -28,14 +28,13 @@ class PID:
         return np.clip((self.kp * e) + (self.kd * de) + (self.ki * ie), 0.0, 1.0)
     
 class PIDController:
-    def __init__(self, learning_rate, tc, min_distance_offset, buffer_size = 42, kp_velocity = 0.4, ki_velocity = 0.2, kd_velocity = 0.01, 
+    def __init__(self, learning_rate, tc, buffer_size = 42, kp_velocity = 0.4, ki_velocity = 0.2, kd_velocity = 0.01, 
                 kp_distance = 1.6, ki_distance = 0.01, kd_distance = 0.7):
         self.pid_velocity = PID(learning_rate, tc, buffer_size, kp_velocity, ki_velocity, kd_velocity)
         self.pid_distance = PID(learning_rate, tc, buffer_size, kp_distance, ki_distance, kd_distance)
-        self.min_distance_offset = min_distance_offset
 
     def apply_control(self, control_info, target_velocity, current_velocity, min_depth):
-        min_permitted_distance = carla_utility.compute_security_distance(current_velocity) + self.min_distance_offset
+        min_permitted_distance = carla_utility.compute_security_distance(current_velocity) + control_info.min_permitted_offset
         distance_error = min_depth - min_permitted_distance
         
         if distance_error > 0:
