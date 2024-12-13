50cd53b2f297866444cb0b7fa5e74b1c39c899a4
Luigi Borriello - luigi.borriello2@studio.unibo.it <luigi.borriello2@studio.unibo.it>
2024-12-13 17:33:50 +0100

    refactor: make min-offset flexible

--- manual_control.py
+++ manual_control.py
@@ -1,17 +1,22 @@
 import pygame, carla
 
 class ControlInfo:
-    def __init__(self, pid_cc=False, ego_control=carla.VehicleControl(), target_control=carla.VehicleControl()):
+    def __init__(self, pid_cc=False, ego_control=carla.VehicleControl(), target_control=carla.VehicleControl(), min_permitted_offset=7):
         self.pid_cc = pid_cc
         self.ego_control = ego_control
         self.target_control = target_control
         self.running = True
+        self.min_permitted_offset = min_permitted_offset
         
 
     def __str__(self):
         return f"ego_control: {self.ego_control}, target_control: {self.target_control}, pid_cc: {self.pid_cc}, running: {self.running}"
 
+    def reset_ego_control(self):
+        self.ego_control = carla.VehicleControl()
+
 def compute_control(control_info: ControlInfo):
+
     for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.QUIT or event.key == pygame.K_ESCAPE:
@@ -33,11 +38,20 @@ def compute_control(control_info: ControlInfo):
         control_info.pid_cc = not control_info.pid_cc    
     if keys[pygame.K_UP]:
         control_info.ego_control.throttle = 1.0
+        control_info.ego_control.brake = 0.0
     if keys[pygame.K_DOWN]:
         control_info.ego_control.brake = 1.0
+        control_info.ego_control.throttle = 0.0
         control_info.pid_cc = False
     if keys[pygame.K_LEFT]:
         control_info.ego_control.steer = -0.38
     if keys[pygame.K_RIGHT]:
         control_info.ego_control.steer = 0.38
+    if keys[pygame.K_PLUS]:
+        print("Increasing min permitted distance")
+        if control_info.min_permitted_offset == 7 or control_info.min_permitted_offset == 14:
+            control_info.min_permitted_offset += 7
+        else:
+            control_info.min_permitted_offset = 7
+
     
